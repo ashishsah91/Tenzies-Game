93ab5cc40ce42405b6750823e60633efa5f84529
Ashish Sah <ashishsah@gmail.com>
2025-11-05 21:30:23 +0100

    Add game win functionality

--- tenzies-app/src/App.jsx
+++ tenzies-app/src/App.jsx
@@ -2,31 +2,38 @@ import './index.css'
 import Dice from './components/Dice'
 import React from 'react'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
 
-  // Intialize  dice array  with 10 items 
-  const diceArray = new Array(10).fill(0).map((ele) => {
-    return { 'isSelect': false, 'value': ele = Math.ceil(Math.random() * 6) }
-  }
-  )
+  const [diceList, setDiceList] = React.useState(() => generateAllNewDice())
 
-  const [diceList, setDiceList] = React.useState(diceArray)
-
-  console.log(diceList);
   // Dice Array Element intialized 
   const diceArrayElement = diceList.map((dice, index) =>
-    <Dice key={nanoid()} value={dice.value} handleDiceSelect={() => handleDiceSelect(index)} isSelect={dice.isSelect} />)
+    <Dice key={dice.id} value={dice.value} handleDiceSelect={() => handleDiceSelect(index)} isSelect={dice.isSelect} />)
 
+
+  // Game won variable defined
+  const gameWon = diceList.every((ele) => ele.isSelect) && diceList.every((ele) => ele.value === diceList[0].value);
+
+
+  // Generate All new dice 
+  function generateAllNewDice() {
+    return new Array(10).
+      fill(0).
+      map((ele) => (
+        { 'id': nanoid(), 'isSelect': false, 'value': ele = Math.ceil(Math.random() * 6) }
+      ))
+  }
 
   // Dice roll function
   function handleDiceRoll() {
-
-    setDiceList(function (prevValue) {
-      return prevValue.map((ele) =>
-        (ele.isSelect) ? ele : { ...ele, value: Math.ceil(Math.random() * 6) }
-      )
-    })
+    (gameWon) ? setDiceList(generateAllNewDice()) :
+      setDiceList(function (prevValue) {
+        return prevValue.map((ele) =>
+          (ele.isSelect) ? ele : { ...ele, value: Math.ceil(Math.random() * 6) }
+        )
+      })
   }
 
   function handleDiceSelect(ind) {
@@ -39,7 +46,10 @@ function App() {
 
   return (
     <main>
+      {gameWon && <Confetti />}
+
       <section className="header-section">
+        {gameWon && <h3>Congratulations, You WON</h3>}
         <h1>Tenzies</h1>
         <p>Roll until all dices are same. Click each dice to freeze it at its current value between rolls.</p>
       </section>
@@ -47,7 +57,7 @@ function App() {
         {diceArrayElement}
       </section>
       <section className="btn-section">
-        <button onClick={handleDiceRoll}>Roll</button>
+        <button onClick={handleDiceRoll}>{(gameWon) ? 'New Game' : 'Roll'}</button>
       </section>
     </main>
 
